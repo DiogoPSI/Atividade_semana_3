bf0aa65c33ca54313d78789502a93f1ba3ec76ec
DiogoPSI <diogo.2002.miguel@gmail.com>
2020-06-19 16:20:02 +0100

    atividade semana 3

---
+++ app.py
@@ -0,0 +1,38 @@
+from random import *
+
+num = 20
+num1 = "30"
+string = "gestao e programacao de sistemas informaticos"
+string1 = "Ola "
+string2 = "Mundo"
+
+# print(str(num) + " dias")
+# print(int(num1) + 2)
+# print(pow(3, 2))
+# print(max(3, 2))
+# print(min(3, 2))
+# print(round(3.6))
+# print(divmod(10, 3))
+
+# print(string.index("t"))
+# print(string.replace("o", "i"))
+# print(string.upper())
+# print(string.lower())
+# print(len(string))
+# print(string.split())
+# string3 = string1 + string2
+# print(string[12:20])
+# print(string[12:])
+# print(string[:20])
+# string = string.replace("gestao ", "")
+
+# print(random())
+# print(uniform(2.5, 20.3))
+# print(randint(1, 10))
+# print(randrange(1, 10))
+
+# string = string.split()
+# print(string)
+# shuffle(string)
+# print(choice(string))
+# print(string)
